37d108fb19bc2c5bb98f288e83a1d3f6dae8d605
Juan Lagunas <juanlagunas28@gmail.com>
2023-07-03 06:42:50 -0400

    Change header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 		<div className="h-full w-full flex flex-col items-center justify-between overflow-hidden">
 			<div className="flex items-center h-12">
 				<Link href="" className="text-lg font-semibold">
-					Juan&#39;s Tutorial
+					Juan Tutorial
 				</Link>
 			</div>
 			<ModernHouse />
