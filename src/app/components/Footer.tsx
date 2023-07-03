d79c5110dff338768320119b0e64d115a8b2457c
Juan Lagunas <juanlagunas28@gmail.com>
2023-07-03 06:16:22 -0400

    Initial commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import Link from 'next/link'
+import { useState } from 'react'
+
+const Footer = () => {
+	const [active, setActive] = useState(false)
+	const [navActive, setNavActive] = useState('')
+	const nav = [
+		{
+			name: 'Structure',
+			subNav: ['Foundation', 'Frame', 'Roof'],
+		},
+		{
+			name: 'Functional',
+			subNav: ['Windows/Doors', 'Plumbing', 'Electrical', 'HVAC'],
+		},
+		{
+			name: 'Finishes',
+			subNav: [
+				'Siding',
+				'Drywall and Paint',
+				'Interior Finish',
+				'Finished Flooring',
+			],
+		},
+	]
+
+	return (
+		<div className="relative w-11/12 sm:w-3/4 h-14 mb-4 rounded-full bg-[#261F1B] grid grid-flow-col gap-2 items-center text-center p-2 transition shadow-2xl">
+			{nav.map((item) => (
+				<>
+					<div
+						onClick={() => {
+							if (navActive == item.name) {
+								setActive(!active)
+							} else {
+								setActive(true)
+							}
+							setNavActive(item.name)
+						}}
+						className={`${
+							active && navActive == item.name ? 'bg-[#8C6954]' : ''
+						} relative group hover:bg-[#8C6954] px-2 py-2 rounded-full cursor-pointer`}
+					>
+						{item.name}
+						<div
+							className={`
+              ${active && navActive == item.name ? '' : 'hidden'}
+              max-lg:hidden
+              group-hover:visible
+              absolute
+              h-32
+              w-full
+              bg-[#261F1B]
+              left-0
+              -top-36
+              rounded-3xl
+              grid
+              grid-cols-2
+              gap-2
+              p-2
+            `}
+						>
+							{item.subNav.map((item) => (
+								<Link
+									href="/"
+									className="flex justify-center items-center rounded-full hover:text-[#F29F05]"
+								>
+									{item}
+								</Link>
+							))}
+						</div>
+					</div>
+
+					<div
+						className={`
+              ${active && navActive == item.name ? '' : 'hidden'}
+              lg:hidden
+              group-hover:visible
+              absolute
+              h-32
+              w-full
+              bg-[#261F1B]
+              -top-36
+              rounded-3xl
+              grid
+              grid-cols-2
+              gap-2
+              p-2
+            `}
+					>
+						{item.subNav.map((item) => (
+							<Link
+								href="/"
+								className="flex justify-center items-center rounded-full hover:text-[#F29F05]"
+							>
+								{item}
+							</Link>
+						))}
+					</div>
+				</>
+			))}
+		</div>
+	)
+}
+
+export default Footer
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import Footer from './components/Footer'
+import ModernHouse from './components/modern/ModernHouse'
+
+export default function Home() {
+	return (
+		<div className="h-full w-full flex flex-col items-center justify-between overflow-hidden">
+			<div className="flex items-center h-12">
+				<Link href="" className="text-lg font-semibold">
+					PBJ's Tutorials
+				</Link>
+			</div>
+			<ModernHouse />
+			<Footer />
+		</div>
+	)
+}
